eb4f6b7635ff4ed893e06bf5643d7fc10c266f89
Joel Klint <joel.klint@gmail.com>
2023-10-25 19:36:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nx-aws-cdk
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-aws-cdk` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-aws-cdk` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ jest.config.lib.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-aws-cdk',
+  preset: './jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nx-aws-cdk',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
